92b449b673b8fc31940af7d717b2dab81bc5edcd
rojer <rojer@rojer.me>
2016-11-28 02:13:50 +0000

    Make in_buf and out_buf dynamic
    in_buf is fairly straightforward: grow by as much as we want to read, only a few places need to be updated.
    
    out_buf is more tricky because it is grown gradually, as things are being added
    to messages and in various places pointers into buffer are held and need to be
    updated in case realloc moves the buffer.
    To keep track of the pointers we maintain a "buffer context" struct
    and take it along with the pointers while building the message.
    Most of the places have been modified, with notable exception of
    signing and ECDH functions, for these we just take a reasonable guess at
    output: 600 bytes, which should be enough for 2048 bit RSA keys or EC
    groups. Should be enough for everyone, right? Right?
    Well, anyway, we can get back to this later :)
    
    Note: compression is not supported and DTLS mode has not been tested.

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -1463,7 +1463,8 @@
  *
  * Comment this macro to disable support for DTLS
  */
-#define MBEDTLS_SSL_PROTO_DTLS
+/* TODO(rojer): Make dynamic buffers work with DTLS */
+//#define MBEDTLS_SSL_PROTO_DTLS
 
 /**
  * \def MBEDTLS_SSL_ALPN
@@ -1487,7 +1488,7 @@
  *
  * Comment this to disable anti-replay in DTLS.
  */
-#define MBEDTLS_SSL_DTLS_ANTI_REPLAY
+//#define MBEDTLS_SSL_DTLS_ANTI_REPLAY
 
 /**
  * \def MBEDTLS_SSL_DTLS_HELLO_VERIFY
@@ -1505,7 +1506,7 @@
  *
  * Comment this to disable support for HelloVerifyRequest.
  */
-#define MBEDTLS_SSL_DTLS_HELLO_VERIFY
+//#define MBEDTLS_SSL_DTLS_HELLO_VERIFY
 
 /**
  * \def MBEDTLS_SSL_DTLS_CLIENT_PORT_REUSE
@@ -1521,7 +1522,7 @@
  *
  * Comment this to disable support for clients reusing the source port.
  */
-#define MBEDTLS_SSL_DTLS_CLIENT_PORT_REUSE
+//#define MBEDTLS_SSL_DTLS_CLIENT_PORT_REUSE
 
 /**
  * \def MBEDTLS_SSL_DTLS_BADMAC_LIMIT
@@ -1532,7 +1533,7 @@
  *
  * Requires: MBEDTLS_SSL_PROTO_DTLS
  */
-#define MBEDTLS_SSL_DTLS_BADMAC_LIMIT
+//#define MBEDTLS_SSL_DTLS_BADMAC_LIMIT
 
 /**
  * \def MBEDTLS_SSL_SESSION_TICKETS
@@ -1547,6 +1548,7 @@
  * Comment this macro to disable support for SSL session tickets
  */
 #define MBEDTLS_SSL_SESSION_TICKETS
+#define MBEDTLS_SSL_MAX_SESSION_TICKET_LEN 128
 
 /**
  * \def MBEDTLS_SSL_EXPORT_KEYS
